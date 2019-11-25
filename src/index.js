994c54776c276007ddaf940fae5930f8eb58f2af
Valera Vladyko <amintas42@gmail.com>
2019-11-25 11:26:37 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -260,8 +260,8 @@ class SuperTreeview extends Component {
                     ? this.printNoChildrenMessage()
                     : nodeArray.map((node, index) => {
                           const nodeText = get(node, keywordLabel, '');
-                          const teststyles = node.getStyleClassCb();
-                          
+                          const teststyles = typeof node.getStyleClassCb === "function" ? node.getStyleClassCb() : '';
+
                           return (
                               <CSSTransition
                                   {...nodeTransitionProps}
